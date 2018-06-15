1c7761977b5b41c9f060e571cbe73b6c5b744405
Sergey Slotin <slotserg@gmail.com>
2018-06-15 20:52:28 +0300

    cuda + torch 0.4

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ import time
 
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
-from torch.autograd import Variable # depricated, need to update and rewrite
 import torch.autograd as autograd
 
 from datasets import FacesDataset, AnimeDataset
@@ -22,25 +21,25 @@ parser.add_argument('--epoch', type=int, default=0,
                     help='epoch to start training from')
 parser.add_argument('--n_epochs', type=int, default=200,
                     help='number of epochs of training')
-parser.add_argument('--batch_size', type=int, default=4,
+parser.add_argument('--batch_size', type=int, default=16,
                     help='size of the batches')
 parser.add_argument('--n_critic', type=int, default=5,
                     help='discriminator training steps per iter')
-parser.add_argument('--sample_interval', type=int, default=5,
+parser.add_argument('--sample_interval', type=int, default=50,
                     help='interval betwen image samples')
 opt = parser.parse_args()
 
 
 cycle_loss = torch.nn.L1Loss()
 
-lambda_adv = 1
+lambda_adv = 3
 lambda_cycle = 10
 lambda_gp = 10
 
-G_A = Generator()
-G_B = Generator()
-D_A = Discriminator()
-D_B = Discriminator()
+G_A = Generator().cuda()
+G_B = Generator().cuda()
+D_A = Discriminator().cuda()
+D_B = Discriminator().cuda()
 
 if opt.epoch != 0:
     G_A.load_state_dict(torch.load('checkpoints/G_A_%d.pth' % opt.epoch))
@@ -57,10 +56,10 @@ optim_D_B = torch.optim.Adam(D_B.parameters())
 
 
 def compute_gradient_penalty(D, real_samples, fake_samples):
-    alpha = torch.FloatTensor(np.random.random((real_samples.size(0), 1, 1, 1)))
-    interpolates = Variable(alpha * real_samples + (1 - alpha) * fake_samples, requires_grad=True)
+    alpha = torch.cuda.FloatTensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+    interpolates = (alpha * real_samples + (1 - alpha) * fake_samples).requires_grad_()
     validity = D(interpolates)
-    fake = Variable(torch.FloatTensor(np.ones(validity.data.shape)), requires_grad=True)
+    fake = (torch.cuda.FloatTensor(np.ones(validity.data.shape))).requires_grad_()
     gradients = autograd.grad(outputs=validity, inputs=interpolates, grad_outputs=fake,
                               create_graph=True, retain_graph=True, only_inputs=True)[0]
     gradients = gradients.view(gradients.size(0), -1)
@@ -69,7 +68,7 @@ def compute_gradient_penalty(D, real_samples, fake_samples):
 
 def sample_images(batches_done): # could break?
     imgs = next(iter(dataloader_A))
-    real_A = Variable(torch.FloatTensor(imgs))
+    real_A = imgs.cuda()
     fake_B = G_B(real_A)
     AB = torch.cat((real_A.data, fake_B.data), -2)
     save_image(AB, 'images/%d.png' % batches_done, nrow=opt.batch_size, normalize=True)
@@ -80,11 +79,11 @@ batches_done = 0
 prev_time = time.time()
 for epoch in range(opt.n_epochs):
     for i, (batch_A, batch_B) in enumerate(zip(dataloader_A, dataloader_B)):
-        print('...', i)
         # shouldn't we get rid of copy-pasting?
-        imgs_A = Variable(torch.FloatTensor(batch_A))
-        imgs_B = Variable(torch.FloatTensor(batch_B))
 
+        imgs_A = batch_A.cuda()
+        imgs_B = batch_B.cuda()
+        
         optim_D_A.zero_grad()
         optim_D_B.zero_grad()
 
@@ -126,9 +125,9 @@ for epoch in range(opt.n_epochs):
             time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time) / opt.n_critic)
             prev_time = time.time()
 
-            print("\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f, cycle: %f] ETA: %s"
-                             % (epoch, opt.n_epochs, i, total, D_loss.data[0],
-                                G_adv.data[0], G_cycle.data[0], time_left))
+            print("Epoch %d/%d | Batch %d/%d | D loss: %f | G loss: %f | cycle: %f | ETA: %s"
+                             % (epoch, opt.n_epochs, i, total, D_loss.item(),
+                                G_adv.item(), G_cycle.item(), time_left))
 
         batches_done += 1
 
