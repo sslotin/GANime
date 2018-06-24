5b9f2f238f7d06ed95a700733aa9758ffd799a37
Sergey Slotin <slotserg@gmail.com>
2018-06-24 20:00:15 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,27 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-# just regular unet-like layers
-
-def Down(in_channels, out_channels, normalize=True, dropout=None):
-    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False)]
-    if normalize:
-        layers += [nn.InstanceNorm2d(out_channels, affine=True)]
-    layers += [nn.LeakyReLU(0.2)]
-    if dropout:
-        layers += [nn.Dropout(dropout)]
-    return nn.Sequential(*layers)
-
-def Up(in_channels, out_channels, dropout=None):
-    layers = [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
-              nn.InstanceNorm2d(out_channels, affine=True),
-              nn.ReLU(inplace=True)]
-    if dropout:
-        layers += [nn.Dropout(dropout)]
-    return nn.Sequential(*layers)
     
 def Generator(channels=3):
+    def Down(in_channels, out_channels, normalize=True, dropout=None):
+        layers = [nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False)]
+        if normalize:
+            layers += [nn.InstanceNorm2d(out_channels, affine=True)]
+        layers += [nn.LeakyReLU(0.2)]
+        if dropout:
+            layers += [nn.Dropout(dropout)]
+        return nn.Sequential(*layers)
+
+    def Up(in_channels, out_channels, dropout=None):
+        layers = [nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+                  nn.InstanceNorm2d(out_channels, affine=True),
+                  nn.ReLU(inplace=True)]
+        if dropout:
+            layers += [nn.Dropout(dropout)]
+        return nn.Sequential(*layers)
+    
     return nn.Sequential(
         Down(channels, 64, normalize=False),
         Down(64, 128),
